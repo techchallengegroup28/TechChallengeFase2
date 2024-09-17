667a734905f06f2011a6379446238036504b159c
Song Shik <song.shik@sa365.ag>
2024-09-17 13:53:17 -0300

    Adicionando a biblioteca para lidar com cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require('cors');
 
 var indexRouter = require("./routes/index");
 var postsRouter = require("./routes/posts");
@@ -12,6 +13,10 @@ var swaggerSpecs = require("./swaggerConfig");
 
 var app = express();
 
+app.use(cors({
+    origin: 'http://localhost:3001',
+    credentials: true, 
+  }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
